1f552b98878a9795e11220ffed7ee93709e631f0
Roberto Parra <uedsoldier1990@gmail.com>
2025-11-19 12:53:55 -0600

    Remove unnecessary sys.path modifications from converter test files

--- tests/converters/test_temperature_converter.py
+++ tests/converters/test_temperature_converter.py
@@ -1,8 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-
 import unittest
 from core.physical_quantities import TemperatureConversionManager
 from core.unit import TemperatureUnits, MockUnits
