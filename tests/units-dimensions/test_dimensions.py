78ab8678b1f97ecd4d1ace93938e9a4320da294f
Roberto Parra <uedsoldier1990@gmail.com>
2025-11-19 01:47:59 -0600

    Refactor dimension validation tests to use direct equality assertions

--- tests/units-dimensions/test_dimensions.py
+++ tests/units-dimensions/test_dimensions.py
@@ -1,8 +1,3 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-
 import unittest
 
 from core.unit import *
@@ -55,11 +50,11 @@ class TestDimensions(unittest.TestCase):
     
     def test_dimensionless1(self):
         test_dim = FORCE_DIMENSIONS/FORCE_DIMENSIONS
-        self.assertEqual(test_dim.is_dimensionless(),True)
+        self.assertEqual(test_dim.is_dimensionless,True)
         
     def test_dimensionless2(self):
         test_dim = CAPACITANCE_DIMENSIONS * FORCE_DIMENSIONS * LENGTH_DIMENSIONS * TIME_DIMENSIONS / (FORCE_DIMENSIONS * LENGTH_DIMENSIONS * TIME_DIMENSIONS * CAPACITANCE_DIMENSIONS)
-        self.assertEqual(test_dim.is_dimensionless(),True)
+        self.assertEqual(test_dim.is_dimensionless,True)
     
     def test_repr1(self):
         print(f'\nInductance dimensions: {repr(INDUCTANCE_DIMENSIONS)}')
@@ -78,27 +73,27 @@ class TestDimensions(unittest.TestCase):
     def test_validate_dimension1(self):
         test_dim = VOLTAGE_DIMENSIONS**2/RESISTANCE_DIMENSIONS
         expected_dim = POWER_DIMENSIONS
-        self.assertTrue(test_dim.validate_dimension(expected_dim))
+        self.assertEqual(test_dim,expected_dim)
     
     def test_validate_dimension2(self):
         test_dim = MASS_DIMENSIONS * SPEED_DIMENSIONS / TIME_DIMENSIONS
         expected_dim = FORCE_DIMENSIONS
-        self.assertTrue(test_dim.validate_dimension(expected_dim))
+        self.assertEqual(test_dim,expected_dim)
     
     def test_validate_dimension3(self):
         test_dim = RESISTANCE_DIMENSIONS*CAPACITANCE_DIMENSIONS
         expected_dim = TIME_DIMENSIONS
-        self.assertTrue(test_dim.validate_dimension(expected_dim))
+        self.assertEqual(test_dim,expected_dim)
     
     def test_validate_dimension4(self):
         test_dim = INDUCTANCE_DIMENSIONS / RESISTANCE_DIMENSIONS
         expected_dim = TIME_DIMENSIONS
-        self.assertTrue(test_dim.validate_dimension(expected_dim))
+        self.assertEqual(test_dim,expected_dim)
     
     def test_validate_dimension5(self):
         test_dim = POWER_DIMENSIONS / (LENGTH_DIMENSIONS * TEMPERATURE_DIMENSIONS)
         expected_dim = THERMAL_CONDUCTIVITY_DIMENSIONS
-        self.assertTrue(test_dim.validate_dimension(expected_dim))
+        self.assertEqual(test_dim,expected_dim)
     
         # # Test cases: (input value, input unit, output unit,expected value, decimal places)
         # test_cases = [
