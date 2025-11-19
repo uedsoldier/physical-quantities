1baf842f60f5ec577e489b518f151485019049b6
Roberto Parra <uedsoldier1990@gmail.com>
2025-11-19 01:48:16 -0600

    Remove unnecessary imports from test_units.py

--- tests/units-dimensions/test_units.py
+++ tests/units-dimensions/test_units.py
@@ -1,10 +1,4 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-
 import unittest
-
 from core.unit import *
 
 class TestUnits(unittest.TestCase):
